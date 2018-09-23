ec0b15afc5b1f801820f1f40dec1df3f4808014f
Black Quadrant <dev@blackquadrant.com>
2018-09-24 01:55:24 +0400

    Personal Access Token created

---
+++ tests/ConnectionTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+class ConnectionTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $this->assertSame(API_KEY,'89cDjjYk3ogEX0P/NQnPksFxAPYkspNMIhpWVNG0PoiQUriP8AHpCSynyVXVZ7/f');
+    }
+}
+
+?>
