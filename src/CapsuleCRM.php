eebbcf7281758e9f42d262af1c8535ed30cd6351
Black Quadrant <dev@blackquadrant.com>
2018-09-24 15:50:16 +0400

    TDD Progress

---
+++ src/CapsuleCRM.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace BlackQuadrant\CapsuleCRM;
+
+use GuzzleHttp\Client;
+
+class CapsuleCRM{
+    private $personal_access_token = null;
+    CONST API_ROOT = 'https://api.capsulecrm.com/api/v2/';
+
+    /**
+     * Personal Access Token :: SET
+     */
+    public function set_personal_access_token($personal_access_token){
+        $this->personal_access_token = $personal_access_token;
+        return $this;
+    }
+
+    public function connect(){
+        return $this->call('GET','site',null);
+    }
+
+    private function call( $method, $api_endpoint, $payload ){
+        $client     =   new Client();
+        // Test connection with site settings
+        $response   =   $client->request(strtoupper($method),CapsuleCRM::API_ROOT.$api_endpoint, [
+            'headers' => [
+                'Authorization'=>'Bearer '.$this->personal_access_token   
+            ]
+        ]);
+        return $response;
+    }
+
+    /**
+     * Personal Access Token :: GET
+     */
+    public function get_personal_access_token(){
+        if(is_null($this->personal_access_token)){
+            return "Personal Access Token is not set";
+        }
+        else{
+            return $this->personal_access_token;
+        }
+    }
+}
+
+?>
--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -1,12 +1,28 @@
 <?php 
-
+require_once __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use BlackQuadrant\CapsuleCRM;
 
 class ConnectionTest extends TestCase
 {
-    public function testPushAndPop()
-    {
-        $this->assertSame(API_KEY,'89cDjjYk3ogEX0P/NQnPksFxAPYkspNMIhpWVNG0PoiQUriP8AHpCSynyVXVZ7/f');
+    private $test;
+
+    public function testApiKeyIsPresent(){        
+        // API_KEY constant is set
+        $this->assertTrue(defined('API_KEY'));
+        // API_KEY is not null
+        $this->assertNotNull(API_KEY);
+        // API_KEY is not empty
+        $this->assertNotEmpty(API_KEY);
+    }
+
+    public function testConnectionToCapsuleCrm(){
+        $client     =   new CapsuleCRM\CapsuleCRM();
+        // Test connection with site settings
+        $response   =   $client->set_personal_access_token(API_KEY)->connect();
+        // Successful Connection
+        $this->assertTrue( $response->getStatusCode() === 200 );
     }
 }
 
