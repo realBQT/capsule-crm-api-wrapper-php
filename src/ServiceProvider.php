72bf0ca4c4668713d3c2749feeea5fe7aae123bf
Black Quadrant <dev@blackquadrant.com>
2018-09-28 00:55:41 +0400

    Service Provider config added

---
+++ src/ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BlackQuadrant\CapsuleCRM;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+/**
+ * Class ServiceProvider.
+ */
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'config/api.php' => config_path('blackquadrant/capsule_crm.php'),
+        ]);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
+?>
